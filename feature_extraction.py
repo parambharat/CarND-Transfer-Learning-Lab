49da8496469cf758f771d5a775236a4003437a73
Bharat <param.bharat@gmail.com>
2016-12-25 12:21:22 +0530

    Initial Commit

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,9 @@
 import pickle
 import tensorflow as tf
+import numpy as np
 # TODO: import Keras layers you need here
+from keras.models import Model
+from keras.layers import Dense, Flatten, Input
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -8,7 +11,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
-
+flags.DEFINE_integer('epochs', '', "Number of epochs to run")
+flags.DEFINE_integer('batch_size', '', "Batch size")
 
 def load_bottleneck_data(training_file, validation_file):
     """
@@ -36,7 +40,8 @@ def load_bottleneck_data(training_file, validation_file):
 
 def main(_):
     # load bottleneck data
-    X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
+    X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file,
+                                                          FLAGS.validation_file)
 
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
@@ -46,8 +51,26 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    nb_classes = len(np.unique(y_train))
 
     # TODO: train your model here
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(nb_classes, activation='softmax')(x)
+    model = Model(inp, x)
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy']
+                  )
+
+    # train model
+    model.fit(X_train,
+              y_train,
+              nb_epoch=FLAGS.epochs,
+              batch_size=FLAGS.batch_size,
+              validation_data=(X_val, y_val),
+              shuffle=True)
 
 
 # parses flags and calls the `main` function above
